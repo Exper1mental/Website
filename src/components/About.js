5162f85ff6ddc6d3d8b1a8ed0f2ef4f70392fdb4
Thomas Delvaux <Exper1mental@users.noreply.github.com>
2023-02-12 18:26:52 -0500

    Update About.js
    Added alt text descriptions to the images used in the About section.

--- src/components/About.js
+++ src/components/About.js
@@ -9,21 +9,21 @@ function About() {
             <h2 className="text-center text-xl font-bp;d text-gray-300">About Me</h2>
             <ul className="grid p-10 gap-10 md:grid-cols-3">
                 <li className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-gray-900 shadow rounded-lg overflow-hidden">
-                    <img src={Hike} alt="discovery" className="w-full" />
+                    <img src={Hike} className="w-full" alt="A picture of a couple hiking in the Swiss Alps." />
                     <div className="p-6 md:p-8 text-gray-300">
                         <h3 className="font-bold">Hiking</h3>
                         <p>I love discovering places I've never been before.</p>
                     </div>
                 </li>
                 <li className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-gray-900 shadow rounded-lg overflow-hidden">
-                    <img src={Coding} className="w-full" />
+                    <img src={Coding} className="w-full" alt="A picture of a computer with with programming a text editor." />
                     <div className="p-6 md:p-8 text-gray-300">
                         <h3 className="font-bold">Computer Programming</h3>
                         <p>One of my favorite topics to learn about in my free time.</p>
                     </div>
                 </li>
                 <li className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bg-gray-900 shadow rounded-lg overflow-hidden">
-                    <img src={Gunter} className="w-full" />
+                    <img src={Gunter} className="w-full" alt="A picture of my german shepherd in the snow."/>
                     <div className="p-6 md:p-8 text-gray-300">
                         <h3 className="font-bold">Gunter</h3>
                         <p>My favorite german shepherd!</p>
